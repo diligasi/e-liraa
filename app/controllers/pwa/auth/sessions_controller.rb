7e8725b6f850a86c4ec63903a4a4a7c89e4c00aa
Diogo de Lima <diligasi@gmail.com>
2020-12-06 01:19:24 -0300

    Add cookies control for sign in and sign out

--- app/controllers/pwa/auth/sessions_controller.rb
+++ app/controllers/pwa/auth/sessions_controller.rb
@@ -3,11 +3,23 @@ module Pwa::Auth
     layout 'pwa_sign_in'
 
     def after_sign_in_path_for(resource)
+      set_user
       stored_location_for(resource) || app_root_path
     end
 
     def after_sign_out_path_for(_)
+      clean_user
       new_user_session_path
+    end
+
+    private
+
+    def set_user
+      cookies[:user_id] = current_user.id
+    end
+
+    def clean_user
+      cookies.delete(:user_id)
     end
   end
 end
