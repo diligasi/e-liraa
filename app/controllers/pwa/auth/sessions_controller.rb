25de9677411c785d541389f0bfa3e734f50f122b
Diogo de Lima <diligasi@gmail.com>
2020-12-10 18:41:53 -0300

    Add role sign in control

--- app/controllers/pwa/auth/sessions_controller.rb
+++ app/controllers/pwa/auth/sessions_controller.rb
@@ -2,6 +2,21 @@ module Pwa::Auth
   class SessionsController < Devise::SessionsController
     layout 'pwa_sign_in'
 
+    def create
+      self.resource = warden.authenticate!(auth_options)
+
+      if self.resource.field?
+        set_flash_message!(:notice, :signed_in)
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
+      else
+        sign_out
+        set_flash_message!(:alert, :access_denied)
+        redirect_to new_user_session_path
+      end
+    end
+
     def after_sign_in_path_for(resource)
       set_user
       stored_location_for(resource) || app_root_path
