8c801291752ce64c2ed582f89cb3ecf057761b1a
Diogo de Lima <diligasi@gmail.com>
2020-12-13 17:24:06 -0300

    Add staging host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,14 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :user_name => ENV['MAILTRAP_USER'],
-  :password => ENV['MAILTRAP_PASSWORD'],
-  :address => 'smtp.mailtrap.io',
-  :domain => 'smtp.mailtrap.io',
-  :port => '2525',
-  :authentication => :cram_md5
-}
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['MAILTRAP_USER'],
+    :password => ENV['MAILTRAP_PASSWORD'],
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+  config.action_mailer.default_url_options = { :host => 'e-liraa-stg.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
