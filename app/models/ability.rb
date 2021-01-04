53d1b2b0a6dc5bd08b53bd9f9864b08f4eadc602
Diogo de Lima <diligasi@gmail.com>
2021-01-04 01:52:15 -0300

    Remove comments from ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,8 +8,6 @@ class Ability
 
     return can :manage, :all if user.admin?
 
-    alias_action :create, :read, :update, :destroy, to: :crud
-
     if user.field?
       can :manage, User, id: user.id
       can %i[index create read], FieldForm, user_id: user.id
@@ -27,32 +25,5 @@ class Ability
       can %i[index read], [Faq, Institutional]
       can %i[index read], FieldForm, user: { departament_id: user.departament_id }
     end
-
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
