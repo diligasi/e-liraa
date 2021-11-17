41dab182525eab77624d2caf0e09a661bd4508ff
Diogo de Lima <diligasi@gmail.com>
2021-11-17 14:18:56 -0300

    Update ruby and rails

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# Welcome to VigiAedes!
+# Welcome to OlhAedes!
 
 Hi! For a brief context, this is a project with a scientific focus and with the purpose of assisting in pest control programs in the real world, more precisely, the LIRAa program.
 
 The "Levantamento de Índice Rápido para *Aedes aegypti*" (LIR*A*a) is an official guidance already approved and in use that aims to provide entomoepidemiological survey indexes for a quick and timely mapping of the possible progress of Aedes aegypti in order to direct the appropriate actions to be taken.
 
-The **VigiAedes** project aims to make the LIR*A*a process more efficient by abandoning its analogue mode and computerizing the entire flow, from the data collected by the field agents to their laboratory analysis.
+The **OlhAedes** project aims to make the LIR*A*a process more efficient by abandoning its analogue mode and computerizing the entire flow, from the data collected by the field agents to their laboratory analysis.
 
 # Modules
 
 This application is basically divided into 3 modules:
 
- 1. PWA - Progressive Web App
-	 - Application used by field agents for data collection.
- 2. Admin
-	 - Application used to manage the collected data, check out reports, etc.
- 3. API
-	 - Application to support and processes all the information needed by both the admin and pwa  applications
+1. PWA - Progressive Web App
+	- Application used by field agents for data collection.
+2. Admin
+	- Application used to manage the collected data, check out reports, etc.
+3. API
+	- Application to support and processes all the information needed by both the admin and pwa  applications
 
 # Dependencies
 
-- Ruby Version  `2.7.1`
-- Rails Version  `6.0.3`
+- Ruby Version  `3.0.2`
+- Rails Version  `6.1.4.1`
 - Database  `PostgreSQL`
 - Rails Server  `Puma`
 - Webpacker  `4.0`
@@ -30,7 +30,7 @@ This application is basically divided into 3 modules:
 
 1. Clone the project
 ```bash
-$ git clone git@github.com:diligasi/e-liraa.git
+$ git clone git@github.com:diligasi/olh_aedes.git
 ```
 
 2. Get the credential keys from the project manager and put the `master.key` on the `config/` folder. The other keys should be put on the `config/credentials` folder.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,50 @@
-# Departament
-Departament.find_or_create_by(name: 'Secretaria #01', description: '')
-Departament.find_or_create_by(name: 'Secretaria #02', description: '')
+# Departament ========================================================================================
+department = { name: 'Secretaria Municipal de Saúde de Niterói', description: 'www.saude.niteroi.rj.gov.br - Rua Visconde de Sepetiba, 987, 8° andar - Niterói - Tel: (21) 2716-5800 - Designer: C. S. Rodrigues' }
+current_departament = Department.find_or_create_by(department)
 
-# PropertyTypes
-PropertyType.find_or_create_by(name: 'Apartamento', description: '')
-PropertyType.find_or_create_by(name: 'Casa', description: '')
-PropertyType.find_or_create_by(name: 'Prédio', description: '')
+# Regions ============================================================================================
+# [
+#   { name: '1A', department: current_departament },
+#   { name: '1B', department: current_departament },
+#   { name: '2A', department: current_departament },
+#   { name: '2B', department: current_departament },
+#   { name: '3A', department: current_departament }
+# ].each do |region|
+#   Region.find_or_create_by(region)
+# end
 
-# ShedTypes
-ShedType.find_or_create_by(name: "CX D'água", description: '')
-ShedType.find_or_create_by(name: 'Nível Solo', description: '')
-ShedType.find_or_create_by(name: 'Dep. Pequeno', description: '')
+# PropertyTypes ======================================================================================
+[
+  { name: 'Residência', description: 'Moradia fixa em determinado lugar; casa, apartamento.' },
+  { name: 'Terreno Baldio', description: 'Terreno abandonado ou sem dono.' },
+  { name: 'Comércio', description: 'Estabelecimento comercial seja ele para a venda de produtos físicos ou serviços.' },
+  { name: 'Ponto Estratégico', description: '' },
+  { name: 'Outros', description: '' }
+].each do |property_type|
+  PropertyType.find_or_create_by(property_type)
+end
 
-# larva_species
-LarvaSpecy.find_or_create_by(name: 'Aedes Aegypti', description: '')
-LarvaSpecy.find_or_create_by(name: 'Aedes albopictus', description: '')
+# ShedTypes ==========================================================================================
+[
+  { name: "CX D'água", description: 'Tanque destinado a armazenar água para consumo humano, agrícola ou qualquer outra função que requeira o uso da mesma.' },
+  { name: 'Depósito Fixo', description: '' },
+  { name: 'Depósito Natural', description: '' },
+  { name: 'Dep. Pequeno', description: '' },
+  { name: 'Lixo (depositos)', description: '' },
+  { name: 'Nível Solo', description: '' },
+  { name: 'Pneus', description: '' }
+].each do |shed_type|
+  ShedType.find_or_create_by(shed_type)
+end
 
-# institutionals
+# LarvaSpecies =======================================================================================
+[
+  { name: 'Aedes Aegypti', description: '' },
+  { name: 'Aedes albopictus', description: '' },
+  { name: 'Outras', description: '' }
+].each do |larva_specy|
+  LarvaSpecy.find_or_create_by(larva_specy)
+end
+
+# Institutionals =====================================================================================
 Institutional.find_or_create_by(phone_numbers: '(21) XXXXX-XXXX', description: 'Tornar o processo de Levantamento de Índice Rápido para Aedes aegypti (LIRAa) mais eficiente e preciso em seu mapeamento de regiões afetadas, qualidade dos dados coletados, redução das perdas de informação durante as coletas e facilitar o acesso e divulgação posterior dos resultados.')
