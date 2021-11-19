f8ae8e194e02cde67ab14df7fa64b8d33294982b
Diogo de Lima <diligasi@gmail.com>
2021-11-19 02:19:03 -0300

    Update department's views according to region relation

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,6 +4,6 @@ class Department < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def number_of_active_users
-    users.where(users: { status: true }).size
+    regions.joins(:users).where(users: { status: true }).size
   end
 end
